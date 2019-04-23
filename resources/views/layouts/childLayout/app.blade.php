b1f7b24f5faf0905b2ca524861ff729949212175
zilandemirell <zilandemirell@gmail.com>
2019-04-23 12:51:38 +0300

    child layout and child home added

---
+++ resources/views/layouts/childLayout/app.blade.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr" xmlns="">
+  <head>
+
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+      <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+      <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+      <script src="assets/js/jquerycounter.js"></script>
+
+      <!-- styles  -->
+
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+    <link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
+    <link href="assets/css/docs.css" rel="stylesheet">
+    <link href="assets/js/google-code-prettify/prettify.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,300|Open+Sans:400,300,300italic,400italic" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
+
+    <link href="assets/color/success.css" rel="stylesheet">
+    <link href="assets/css/prettyPhoto.css" rel="stylesheet" />
+
+
+    <!-- fav and touch icons -->
+    <link rel="shortcut icon" href="assets/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="assets/ico/apple-touch-icon-57-precomposed.png">
+    <title></title>
+  </head>
+  <body>
+    @include('layouts.childLayout.navbar')
+
+  <div class="container">
+      @yield('content')
+  </div>
+
+    <script src="js/jquery.js"></script>
+    <script src="js/jquery.easing.1.3.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/modernizr.custom.js"></script>
+    <script src="js/toucheffects.js"></script>
+    <script src="js/google-code-prettify/prettify.js"></script>
+    <script src="js/jquery.prettyPhoto.js"></script>
+    <script src="js/portfolio/jquery.quicksand.js"></script>
+    <script src="js/portfolio/setting.js"></script>
+    <script src="js/animate.js"></script>
+
+    <!-- Template Custom JavaScript File -->
+    <script src="js/custom.js"></script>
+
+    <script src="js/calendarCreate.js"></script>
+
+
+  </body>
+
+
+</html>
