9ab9aa062929122e34f5df0e67e519baf84c72f2
eniseirem <eniseirem@gmail.com>
2019-05-08 12:17:15 +0300

    -saved succesfully

--- app/transaction.php
+++ app/transaction.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class transaction extends Model
 {
+    protected $fillable = [
+        'user_id', 'date', 'balance', 'hour', 'created_at', 'isFailed'
+    ];
+
 
     public function user()
     {
